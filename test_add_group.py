f7821c8a3c2a6355ed756e44b25c78b9a8a372cc
shemshedinova <shemshedinovaadile2017@gmail.com>
2021-11-02 16:14:01 +0200

    test_add_group added to pycharm extra strings removed driver modified => wd

--- test_add_group.py
+++ test_add_group.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest
+import unittest, time, re
 
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
@@ -24,15 +27,17 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("test")
+        wd.find_element_by_name("group_name").send_keys("adile")
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("test")
+        wd.find_element_by_name("group_header").send_keys("adile")
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("test")
+        wd.find_element_by_name("group_footer").send_keys("adile")
         wd.find_element_by_name("submit").click()
+        wd.find_element_by_link_text("group page").click()
         wd.find_element_by_link_text("Logout").click()
+        wd.find_element_by_id("LoginForm").click()
     
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
