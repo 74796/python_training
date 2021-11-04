e1437d60cdf539c8ff0f03291bf977bf8396e938
shemshedinova <shemshedinovaadile2017@gmail.com>
2021-11-04 17:10:37 +0200

    Задание 4

--- test_add_group.py
+++ test_add_group.py
@@ -12,13 +12,12 @@ def app(request):
 
 
 def test_add_group(app):
-        app.login(username="admin", password="secret")
-        app.create_group(Group(name="adile", header="adile", footer="adile"))
-        app.logout()
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="adile", header="adile", footer="adile"))
+    app.logout()
 
 
 def test_add_empty_group(app):
-        app.login(username="admin", password="secret")
-        app.create_group(Group(name="", header="", footer=""))
-        app.logout()
-
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
