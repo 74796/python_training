8c2b3790f1401fc78edf10393e939b30b86df06b
shemshedinova <shemshedinovaadile2017@gmail.com>
2021-11-05 14:17:36 +0200

    Задание 5  - Конечный результат Создана двухуровневая архитектура тестов

--- fixture1/application.py
+++ fixture1/application.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import Select
 from fixture1.session import SessionHelper
+from fixture1.contact import ContactHelper
 
 
 class Application:
@@ -9,59 +9,11 @@ class Application:
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
+        self.contact = ContactHelper(self)
 
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/index.php")
-
-    def open_add_new_contact_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("add new").click()
-
-    def create_contact(self, summ):
-        wd = self.wd
-        # fill address book entry
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(summ.name)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(summ.middlename)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(summ.lastname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(summ.nickname)
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(summ.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(summ.address)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(summ.mobile)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(summ.email)
-        wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(summ.bday)
-        wd.find_element_by_xpath("//option[@value='5']").click()
-        wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(summ.bmonth)
-        wd.find_element_by_xpath("//option[@value='December']").click()
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(summ.byear)
-        wd.find_element_by_name("theform").click()
-        # submit contact creation
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-
-    def return_to_home_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("home page").click()
 
     def destroy(self):
         self.wd.quit()
--- test1/contact_add_group.py
+++ test1/contact_add_group.py
@@ -14,8 +14,8 @@ def app(request):
 def test_contact_add_group(app):
         app.open_home_page()
         app.session.login(username="admin", password="secret")
-        app.open_add_new_contact_page()
-        app.create_contact(Summ(name="Adile", middlename="Shemshedinova", lastname="Revanovna", nickname="Adile", company="Django stars", address="Nauki 62A",
+        app.contact.open_add_new_contact_page()
+        app.contact.create_contact(Summ(name="Adile", middlename="Shemshedinova", lastname="Revanovna", nickname="Adile", company="Django stars", address="Nauki 62A",
                             mobile="+380935121990", email="adileshemshedinovaa@gmail.com", bday="5", bmonth="December", byear="1990"))
-        app.return_to_home_page()
+        app.contact.return_to_home_page()
         app.session.logout()
