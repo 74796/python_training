4b594298aa82dad8c7098e07f63c133d741b256f
shemshedinova <shemshedinovaadile2017@gmail.com>
2021-11-10 15:25:21 +0200

    Задание 7 Cделаны тесты для модификации групп как на уроке

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,7 +1,13 @@
 from model.group_model import Group
 
 
-def test_modify_first_group(app):
+def test_modify_group_name(app):
     app.session.login(username="admin", password="secret")
-    app.group.modify_first_group(Group(name="modified", header="modified", footer="modified"))
+    app.group.modify_first_group(Group(name="New_group"))
+    app.session.logout()
+
+
+def test_modify_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(header="New_header"))
     app.session.logout()
