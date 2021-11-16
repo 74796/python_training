f579c438fe60376579fe6f11605fb49394c14e33
shemshedinova <shemshedinovaadile2017@gmail.com>
2021-11-16 14:22:53 +0200

    Задание 8 Повышена "интеллектуальность" фикстуры: реализована валидация фикстуры перед каждым тестом
    * test_delete_contact - падал => добавила в тест     app.open_home_page()

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -4,10 +4,8 @@ from model.contact_model import Contact
 
 def test_add_contact(app):
     app.open_home_page()
-    app.session.login(username="admin", password="secret")
     app.contact.open_add_new_contact_page()
     app.contact.create_contact(Contact(firstname="Adile", middlename="Revanovna", lastname="Shemshedinova",
                                        nickname="Adile", company="Django stars", address="Nauki 62A",
                                        mobile="+380935121990", email="adileshemshedinovaa@gmail.com"))
     app.contact.return_to_home_page()
-    app.session.logout()
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -3,23 +3,17 @@ from model.contact_model import Contact
 
 def test_modify_contact_name(app):
     app.open_home_page()
-    app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(firstname="New contact name"))
     app.contact.return_to_home_page()
-    app.session.logout()
 
 
 def test_modify_contact_lastname(app):
     app.open_home_page()
-    app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(lastname="New contact lastname"))
     app.contact.return_to_home_page()
-    app.session.logout()
 
 
 def test_modify_contact_middlename(app):
     app.open_home_page()
-    app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(middlename="New contact middlename"))
     app.contact.return_to_home_page()
-    app.session.logout()
